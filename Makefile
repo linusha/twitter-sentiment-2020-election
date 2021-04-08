d7ef7d426b455557ca312bda76993cfb2307aa9b
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-04-08 22:10:40 +0200

    Remove emojis from tweets in one variant

--- Makefile
+++ Makefile
@@ -1,16 +1,20 @@
 
-FILES = clean_tweets.csv orig_tweets.csv data.csv
+FILES = clean_tweets.csv orig_tweets.csv data.csv preprocessed_tweets.csv no_emojis_tweets.csv
 
 backup: $(FILES) 
 	cp clean_tweets.csv clean_tweets.csv.bak
 	cp orig_tweets.csv orig_tweets.csv.bak
 	cp data.csv data.csv.bak
-	tar -cf $(shell date +%s).tar data.csv.bak orig_tweets.csv.bak clean_tweets.csv.bak
-	rm data.csv.bak orig_tweets.csv.bak clean_tweets.csv.bak
+	cp preprocessed_tweets.csv preprocessed_tweets.csv.bak
+	cp no_emojis_tweets.csv no_emojis_tweets.csv.bak
+	tar -cf $(shell date +%s).tar data.csv.bak orig_tweets.csv.bak clean_tweets.csv.bak preprocessed_tweets.csv.bak no_emojis_tweets.csv.bak
+	rm data.csv.bak orig_tweets.csv.bak clean_tweets.csv.bak preprocessed_tweets.csv.bak no_emojis_tweets.csv.bak
 
 
-.PHONY: clean
+.PHONY: clean process_tweets
 
 clean:
-	rm -f clean_tweets.csv orig_tweets.csv data.csv
+	rm -f clean_tweets.csv orig_tweets.csv data.csv no_emojis_tweets.csv preprocessed_tweets.csv
 
+process_tweets:
+	python3 scrape_data_and_analyze_tweets.py ./data/deduplicated_tweets.csv.clean data.csv orig_tweets.csv clean_tweets.csv no_emojis_tweets.csv preprocessed_tweets.csv
--- scrape_data_and_analyze_tweets.py
+++ scrape_data_and_analyze_tweets.py
@@ -4,7 +4,6 @@ import datetime
 import logging
 import re
 import string
-import pdb # TODO: remove
 from nltk.tokenize import TweetTokenizer
 from nltk.stem import PorterStemmer
 
@@ -47,6 +46,16 @@ def remove_urls_from_text(text):
                   r'\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|'
                   r'https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.'
                   r'[a-zA-Z0-9]+\.[^\s]{2,})', '', text)
+
+# https://stackoverflow.com/a/49986645/4418325
+def remove_emoji_from_text(text):
+    regex = re.compile(pattern = "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                           "]+", flags = re.UNICODE)
+    return regrex_pattern.sub(r'',text)
     
 def init_user_activity():
     for entry in ALL_TWEETS_DATA:
@@ -77,16 +86,18 @@ def clean_tweet(tweet):
     tweet = remove_urls_from_text(tweet)
     # remove hashtags but preserve words
     tweet = tweet.replace('#', '')
+    return tweet
 
+def preprocess_tweet(tweet):
     tknzr = TweetTokenizer(strip_handles=True, reduce_len=True, preserve_case=False)
     stmmr = PorterStemmer()
     tweet_tokens = tknzr.tokenize(tweet)
-    tweet_cleaned = []
+    tweet_preprocesesed = []
     for token in tweet_tokens:
         stemmed_token = stmmr.stem(token)
-        tweet_cleaned.append(stemmed_token)
-    tweet_cleaned = ' '.join(tweet_cleaned)
-    return tweet_cleaned
+        tweet_preprocesesed.append(stemmed_token)
+    tweet_preprocesesed = ' '.join(tweet_preprocesesed)
+    return tweet_preprocesesed
 
 def queue_output_for_row(data):
     OUTPUT_DATA_ALL.append({'tweet': data['clean_tweet']})
@@ -100,6 +111,8 @@ INPUT_FILE = sys.argv[1]
 OUTPUT_FILE_DATA = sys.argv[2]
 OUTPUT_FILE_TWEETS_ORIG = sys.argv[3]
 OUTPUT_FILE_TWEETS_CLEAN = sys.argv[4]
+OUTPUT_FILE_TWEETS_CLEAN_NO_EMOJIS = sys.argv[5]
+OUTPUT_FILE_TWEETS_PREPROCESSED = sys.argv[6]
 
 INPUT_FIELDNAMES = ['id',
                     'conversation_id',
@@ -172,6 +185,8 @@ while ALL_TWEETS_DATA:
         HASHTAGS = 0
 
     CLEANED_TWEET = clean_tweet(ROW['tweet'])
+    PEPROCESSED_TWEET = preprocess_tweet(CLEANED_TWEET)
+    NO_EMOJI_TWEET = remove_emoji_from_text(CLEANED_TWEET)
 
     try:
         USER_ACTIVITY_COUNT_FOR_ROW = get_user_activity_for_user(CURR_USER)
@@ -197,6 +212,14 @@ while ALL_TWEETS_DATA:
        'clean_tweet': CLEANED_TWEET,
     })
 
+    preprocessed_tweet_for_row = ({
+       'preprocessed_tweet': PEPROCESSED_TWEET,
+    })
+
+    no_emoji_tweet_for_row = ({
+       'no_emoji_tweet': NO_EMOJI_TWEET,
+    })
+
     # write output with all data except tweet
     with open(OUTPUT_FILE_DATA, 'a', newline='') as o_data:
         DATA_WRITER = csv.DictWriter(o_data, fieldnames=OUTPUT_FIELDS_ALL, delimiter=',')
@@ -211,6 +234,16 @@ while ALL_TWEETS_DATA:
         TWEETS_WRITER = csv.DictWriter(o_clean_tweets, fieldnames=['clean_tweet'], delimiter=',')
         TWEETS_WRITER.writerow(cleaned_tweet_for_row)
 
+    with open(OUTPUT_FILE_TWEETS_PREPROCESSED, 'a', newline='') as o_preprocessed_tweets:
+        TWEETS_WRITER = csv.DictWriter(o_preprocessed_tweets, fieldnames=['preprocessed_tweet'], delimiter=',')
+        TWEETS_WRITER.writerow(preprocessed_tweet_for_row)
+
+    with open(OUTPUT_FILE_TWEETS_CLEAN_NO_EMOJIS, 'a', newline='') as o_no_emojis_tweets:
+        TWEETS_WRITER = csv.DictWriter(o_no_emojis_tweets, fieldnames=['no_emoji_tweet'], delimiter=',')
+        TWEETS_WRITER.writerow(no_emoji_tweet_for_row)
+
 o_data.close()
 o_orig_tweets.close()
 o_clean_tweets.close()
+o_preprocessed_tweets.close()
+o_no_emojis_tweets.close()
