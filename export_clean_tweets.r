15d26861fbdde6f90eb1c55ad019055e77893265
Linus Hagemann <linus.hagemann@student.hpi.de>
2021-06-08 00:33:15 +0200

    Fix export script for cleaned tweets

--- export_clean_tweets.r
+++ export_clean_tweets.r
@@ -1,6 +1,5 @@
 # Small helper script that exports one row of data for analysis on another computer
-library('here')
-here()
-tweet_data <- read.csv(here('complete_data.csv'))
+setwd('~/twitter-sentiment-2020-election/')
+tweet_data <- read.csv('complete_data.csv')
 cleaned_tweets <- tweet_data$clean_tweet
-write.csv(cleaned_tweets, here('clean_tweets.csv'))
+write.csv(cleaned_tweets, 'clean_tweets.csv')
